ad0e603dadd87d14add78d09994d55f811f581e4
Zhenya Nemkevich <ostegoelectric@gmail.com>
2020-02-20 18:26:17 +0300

    docs: create future app options

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Shop } from "../src/components/Shop/index.jsx";
+
+ReactDOM.render(<Shop />, document.getElementById("root"));
