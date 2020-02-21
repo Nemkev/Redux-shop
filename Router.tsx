b8f80ce483e5a697c7e953b94a768a72b1751ded
Zhenya Nemkevich <ostegoelectric@gmail.com>
2020-02-21 20:00:16 +0300

    refactor: change rules

--- Router.tsx
+++ Router.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Shop } from './src/components/Shop/index';
-import { Switch, Route, Redirect } from 'react-router-dom';
+// import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { Wrapper } from './app.style';
 
 export const Routers = () => {
   return (
     <Wrapper>
-      <Switch>
-        <Route exact path="/" component={Shop} />
-        {/* <Redirect from="/" to="shop" /> */}
-      </Switch>
+      {/* <Switch> */}
+      {/* <Route exact path="/" component={Shop} /> */}
+      {/* <Redirect from="/" to="shop" /> */}
+      {/* </Switch> */}
     </Wrapper>
   );
 };
